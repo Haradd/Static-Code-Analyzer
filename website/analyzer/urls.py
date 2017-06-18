cc090ec942142ecf66aa642da240c7f2cd493f07
Haradd <blazej.piaskowski@gmail.com>
2017-06-18 13:19:54 +0000

    added home url

--- website/analyzer/urls.py
+++ website/analyzer/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'analyzer'
 urlpatterns = [
-    url(r'^$', views.reports, name='reports'),
+    url(r'^$', views.upload_form, name='home'),
+    url(r'^reports$', views.reports, name='reports'),
     url(r'^upload$', views.upload_form, name='upload'),
     url(r'^login$', views.login_user, name='login'),
     url(r'^logout$', views.logout_user, name='logout'),
